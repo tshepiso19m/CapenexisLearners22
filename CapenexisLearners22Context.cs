0c6b17077d7776780cf51ea2769cd77034fb916f
TSHEPISO MASIPA <104618398+tshepiso19m@users.noreply.github.com>
2023-01-20 08:07:19 +0200

    Add files via upload

---
+++ CapenexisLearners22Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CapenexisLearners22.Models;
+
+namespace CapenexisLearners22.Data
+{
+    public class CapenexisLearners22Context : DbContext
+    {
+        public CapenexisLearners22Context (DbContextOptions<CapenexisLearners22Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CapenexisLearners22.Models.Facilitator> Facilitator { get; set; } = default!;
+
+        public DbSet<CapenexisLearners22.Models.Learner> Learner { get; set; } = default!;
+
+        public DbSet<CapenexisLearners22.Models.Courses> Course { get; set; } = default!;
+
+        
+    }
+}
